5efc580137f542b5c5924c0ab53d3659d279f20e
hmchithra <33429348+hmchithra@users.noreply.github.com>
2021-02-28 15:02:59 -0800

    Initial commit

---
+++ src/main/java/com/izicap/demo/token/controller/dtos/UserAndPassword.java
@@ -0,0 +1,10 @@
+package com.izicap.demo.token.controller.dtos;
+
+import lombok.Data;
+
+@Data
+public class UserAndPassword {
+
+    private String username;
+    private String password;
+}
---
+++ src/main/java/com/izicap/demo/token/entities/Token.java
@@ -0,0 +1,47 @@
+package com.izicap.demo.token.entities;
+
+import com.fasterxml.jackson.annotation.JsonFormat;
+import lombok.*;
+
+import javax.persistence.*;
+import java.util.Date;
+
+@ToString
+@Entity
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+@EqualsAndHashCode
+public class Token {
+
+	static public String DATE_PATTERN = "yyyy-MM-dd HH:mm:ss";
+	@Id
+	@GeneratedValue(strategy = GenerationType.IDENTITY)
+	@Column(nullable = false)
+	private Long id;
+
+	@Column(name = "token", nullable = false)
+	private String token;
+
+	@Column(name = "creation_date", nullable = false)
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	private Date creationDate;
+
+	@Temporal(TemporalType.TIMESTAMP)
+	@JsonFormat(pattern = "yyyy-MM-dd HH:mm:ss")
+	@Column(name = "exipiration_date", nullable = false)
+	private Date expirationDate;
+
+	@ManyToOne
+	@JoinColumn(name="user_id", nullable=false)
+	private User user;
+
+	@Transient
+	private boolean created = false;
+}
+
+
+
+
